9432457d202ce46b179e905d1340a680466c18e0
Jordan Miller <jmille26@ncsu.edu>
2024-05-30 14:38:20 -0400

    INIT

---
+++ lambdas/src/App.jsx
@@ -0,0 +1,9 @@
+import LambdaNav from './components/LambdaNav';
+
+export default function App() {
+    return (
+        <div>
+            <LambdaNav />
+        </div>
+    )
+}
---
+++ lambdas/src/components/LambdaNav.jsx
@@ -0,0 +1,44 @@
+/**
+ * Everything related to the navigation bar and its properties
+ * are outlined here.
+ * 
+ * Template sourced from https://react-bootstrap.netlify.app/docs/components/navbar/.
+ * 
+ * @author #67 Jordan "InterstellHer" Miller
+ * @since May 30, 2024
+ */
+
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+function LambdaNav() {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default LambdaNav;
